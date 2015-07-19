7afb55f47768e70c81c6d0cca91d85594d5e190f
pokers <pokers11@empal.com>
2015-07-19 22:28:19 +0900

    #5 added mongoose DB
    - Add and connect mongoose DB

---
+++ server/config/config.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Peter on 2015. 7. 19..
+ */
+'use strict';
+
+module.exports = {
+    db: {
+        path: 'mongodb://localhost/todayweather',
+        options: {
+            user: '',
+            pass: ''
+        }
+    }
+}
