66b375a50f87242b0780973ee2cadc262b9a115e
pokers <pokers11@empal.com>
2015-07-21 10:24:55 +0900

    #8 added jwt module
    - Added jwt module and token.controller.js file.
     : It defines the basic functions to use jwt.

--- server/app.js
+++ server/app.js
@@ -7,8 +7,10 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var mgrToken = require('./controllers/token.controller.js');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
