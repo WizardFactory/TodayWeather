3055f342df7ae0b25c6faeafa44ceb9c8ea564d5
Alec Kim <kimalec7@gmail.com>
2015-08-24 17:11:10 +0900

    Fixed #46 update image
    add Helvetica Neue-Thin and Nanum Barun Gothic UltraLight
    delete Nanum Barun Gothic
    tabs style set to standard
    layout 조정 - font size와 img size등을 scss에서  media보고 조정하게 수정.

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -82,5 +82,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/tab/dash');
 
+        $ionicConfigProvider.tabs.style('standard');
         $ionicConfigProvider.tabs.position('bottom');
     });
