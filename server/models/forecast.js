63608b8cf2057adedce522775af5601437980ec8
pokers <pokers11@empal.com>
2015-09-06 08:50:35 +0900

    #10 added the schema of the middle range forecast
    - Added schema for the middle range forecast (target : MVP release)
    - change line align.

--- server/models/forecast.js
+++ server/models/forecast.js
@@ -18,62 +18,85 @@
 var mongoose = require('mongoose');
 
 var bSchema = new mongoose.Schema({
-    town: {first: String, second: String, third: String},
-    coord: {lon: Number, lat: Number},
-    mData: {mCoord:{mx: Number, my: Number},
-            data: {current: Array, short: Array},
-	    cCurr: {time: String, date: String}
-	   }
+	town: {
+		first: String,
+		second: String,
+		third: String
+	},
+	coord: {
+		lon: Number,
+		lat: Number
+	},
+	mData: {
+		mCoord:{
+			mx: Number,
+			my: Number
+		},
+		data: {
+			current: Array,
+			short: Array,
+			shortest: Array,
+			midForecast: Array,
+			midLand: Array,
+			midTemp: Array,
+			midSea: Array
+		},
+		cCurr: {
+			time: String,
+			date: String
+		}
+	}
 });
 
 bSchema.statics = {
-    getData : function (first, second, third, cb){
-	this.find({"town" : { "third" : third, "second" : second, "first" : first }})
-	.exec(cb);
-    },
-    setShortData : function (currentObj, mCoord, cb){
-	var self = this;
+	getData : function (first, second, third, cb){
+		this.find({"town" : { "third" : third, "second" : second, "first" : first }}).exec(cb);
+	},
+	setShortData : function (currentObj, mCoord, cb){
+		var self = this;
 
-	self.findOne({ "mData.mCoord.mx": mCoord.mx, "mData.mCoord.my": mCoord.my })
-	.exec(function(err, res){ 
-            var interval = 0;
-	    var dateInterval = 0;
-	    var length = 0;
+		self.findOne({ "mData.mCoord.mx": mCoord.mx, "mData.mCoord.my": mCoord.my })
+			.exec(function(err, res){
+				var interval = 0;
+				var dateInterval = 0;
+				var length = 0;
 
-	    if(res.mData.cCurr == null || res.mData.cCurr == ""){
-	        interval = 0;
-		dateInterval = 0;
-	    }else{
-	        interval = currentObj[0].time - res.mData.cCurr.time / 300;
-	        dateInterval = currentObj[0].date - res.mData.currentObj[0].date;
-		interval = interval + (8 * dateInterval);
-	    }
-	    self.update({ "mData.mCoord.mx": mCoord.mx, "mData.mCoord.my": mCoord.my },
-	    {$pop : {'mData.data.short' : length - interval}, $addToSet : {'mData.cCurr.time' : currentObj[0].time, 'mData.cCurr.date' : currentObj[0].date}}, 
-	    {safe: true, multi : true, upsert: true}, 
-	    cb);
+				if(res.mData.cCurr == null || res.mData.cCurr == ""){
+					interval = 0;
+					dateInterval = 0;
+				}
+				else{
+					interval = currentObj[0].time - res.mData.oCurr.time / 300;
+					dateInterval = currentObj[0].date - res.mData.currentObj[0].date;
+					interval = interval + (8 * dateInterval);
+				}
 
-            self.update({ "mData.mCoord.mx": mCoord.mx, "mData.mCoord.my": mCoord.my },
-	    {$push : {'mData.data.short' : {$each : currentObj}}}, 
-	    {safe: true, multi : true, upsert: true}, 
-	    cb);
+				self.update({ "mData.mCoord.mx": mCoord.mx, "mData.mCoord.my": mCoord.my },
+					{$pop : {'mData.data.short' : length - interval}, $addToSet : {'mData.cCurr.time' : currentObj[0].time, 'mData.cCurr.date' : currentObj[0].date}},
+					{safe: true, multi : true, upsert: true},
+					cb);
 
-	    // 40 is default array list length 
-            if( length > 40 ) {
-	        self.update({ "mData.mCoord.mx": mCoord.mx, "mData.mCoord.my": mCoord.my },
-    	        {$pop : {'mData.data.short' : -interval}}, 
-    	        {safe: true, multi : true, upsert: true}, 
-    	        cb);
-	    }
-	    console.log('interval : ' + interval);
-	});
-    },
-    setCurrentData : function (currentObj, mCoord, cb){
-	this.update({ "mData.mCoord.mx" : mCoord.mx, "mData.mCoord.my" : mCoord.my },
-	{$push: { "mData.data.short": { $each : currentObj, $slice : -60}}}, 
-	{safe: true, multi : true, upsert: true}, 
-	cb);
-    }
-}
+				self.update({ "mData.mCoord.mx": mCoord.mx, "mData.mCoord.my": mCoord.my },
+					{$push : {'mData.data.short' : {$each : currentObj}}},
+					{safe: true, multi : true, upsert: true},
+					cb);
+
+				// 40 is default array list length
+				if( length > 40 ) {
+					self.update({ "mData.mCoord.mx": mCoord.mx, "mData.mCoord.my": mCoord.my },
+						{$pop : {'mData.data.short' : -interval}},
+						{safe: true, multi : true, upsert: true},
+						cb);
+				}
+				console.log('interval : ' + interval);
+			});
+	},
+	setCurrentData : function (currentObj, mCoord, cb){
+		this.update({ "mData.mCoord.mx" : mCoord.mx, "mData.mCoord.my" : mCoord.my },
+			{$push: { "mData.data.short": { $each : currentObj, $slice : -60}}},
+			{safe: true, multi : true, upsert: true},
+			cb);
+	}
+};
 
 module.exports = mongoose.model('base', bSchema);
