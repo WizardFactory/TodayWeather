c1f6b2243ad4c8a81339a2f6c4bdc24b7f19b357
kay <background.ree@gmail.com>
2015-10-21 23:34:46 +0900

    #202 schema changing follow error

--- server/models/forecast.js
+++ server/models/forecast.js
@@ -2,15 +2,3 @@
  * Created by kay on 2015-10-08.
  */
 
-var current = require('current');
-var short = require('short');
-
-var MODEL = {};
-MODEL.TOWN = 1;
-MODEL.CURRENT = 2;
-
-function setModel(modelCategory){
-    if(modelCategory === 1){ // TOWN
-    }
-}
-
