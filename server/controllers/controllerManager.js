4d538e04edd379e8b853c0ab2b657efd5c060ab4
kay <background.ree@gmail.com>
2015-10-26 23:59:25 +0900

    #155 mid forecast data save mongodb

--- server/controllers/controllerManager.js
+++ server/controllers/controllerManager.js
@@ -9,6 +9,8 @@ var town = require('../models/town');
 //var forecast = require('../models/forecast');
 var short = require('../models/short');
 var current = require('../models/current');
+var midLand = require('../models/midLand');
+var midTemp = require('../models/midTemp');
 var fs = require('fs');
 var config = require('../config/config');
 var convert = require('../utils/coordinate2xy');
@@ -1137,7 +1139,22 @@ Manager.prototype.getMidLand = function(gmt){
             //}
         });
     } else{
-        //
+        var collectShortInfo = new collectTown();
+        collectShortInfo.requestData(collectShortInfo.listAreaCode, collectShortInfo.DATA_TYPE.MID_LAND, key, dateString.date, dateString.time, function(err, dataList) {
+            if (err) {
+                log.error("middle land forecst data recdive faile");
+                return;
+            }
+            log.info('mid Land forecast data receive completed : %d\n', dataList.length);
+
+            dataList.forEach(function(item){
+                //dataFormat.data.push(JSON.parse(JSON.stringify(item.data[0])));
+                midLand.setLandData(item.data[0], item.data[0].regId, function(err, res){
+                    if(err) log.error(err);
+                    log.info(res);
+                });
+            });
+        });
     }
 
 };
@@ -1216,7 +1233,22 @@ Manager.prototype.getMidTemp = function(gmt){
             //}
         });
     } else{
-        //
+        var collectShortInfo = new collectTown();
+        collectShortInfo.requestData(collectShortInfo.listCityCode, collectShortInfo.DATA_TYPE.MID_TEMP, key, dateString.date, dateString.time, function(err, dataList){
+            if (err) {
+                log.error("middle land forecst data recdive faile");
+                return;
+            }
+            log.info('mid Temp forecast data receive completed : %d\n', dataList.length);
+
+            dataList.forEach(function(item){
+                //dataFormat.data.push(JSON.parse(JSON.stringify(item.data[0])));
+                midTemp.setTempData(item.data[0], item.data[0].regId, function(err, res){
+                    if(err) log.error(err);
+                    log.info(res);
+                });
+            });
+        });
     }
 
 };
@@ -1324,21 +1356,21 @@ Manager.prototype.startTownData = function(){
         }
     }, periodValue);
 
-    //self.getTownShortestData();
-    //self.getTownCurrentData();
-    //
-    //var midLoop = setInterval(function(){
-    //    self.getMidLand(9 - (midTimes * 24));
-    //    self.getMidTemp(9 - (midTimes * 24));
-    //    midTimes-=1;
-    //
-    //    if(midTimes < 0 ){
-    //        clearInterval(midLoop);
-    //    }
-    //}, midPeriod);
-    //
-    //self.getMidForecast(9);
-    //self.getMidSea(9);
+    self.getTownShortestData();
+    self.getTownCurrentData();
+
+    var midLoop = setInterval(function(){
+        self.getMidLand(9 - (midTimes * 24));
+        self.getMidTemp(9 - (midTimes * 24));
+        midTimes-=1;
+
+        if(midTimes < 0 ){
+            clearInterval(midLoop);
+        }
+    }, midPeriod);
+
+    self.getMidForecast(9);
+    self.getMidSea(9);
 
     // get short forecast once every three hours.
     self.loopTownShortID = setInterval(function() {
---
+++ server/utils/initMidDataToMongo.js
@@ -0,0 +1,90 @@
+/**
+ * Created by User on 2015-10-26.
+ */
+
+var mongoose = require('mongoose');
+var config = require('../config/config');
+
+mongoose.connect(config.db.path, config.db.options);
+
+var fs = require('fs');
+
+
+var midLandSchema = new mongoose.Schema({
+    town: {
+        first: String,
+    },
+    regId : String
+});
+
+var midTempSchema = new mongoose.Schema({
+    town: {
+        first: String,
+        second: String,
+    },
+    regId : String
+});
+
+var midLandList = [
+    {name: '경기도', code: '11B00000'},
+    {name: '서울특별시', code: '11B00000'},
+    {name: '인천광역시', code: '11B00000'},
+    {name: '강원도 영서', code: '11D10000'},
+    {name: '강원도 영동', code: '11D20000'},
+    {name: '대전광역시', code: '11C20000'},
+    {name: '세종특별자치시', code: '11C20000'},
+    {name: '충청남도', code: '11C20000'},
+    {name: '충청북도', code: '11C10000'},
+    {name: '광주광역시', code: '11F20000'},
+    {name: '전라남도', code: '11F20000'},
+    {name: '전라북도', code: '11F10000'},
+    {name: '대구광역시', code: '11H10000'},
+    {name: '경상북도', code: '11H10000'},
+    {name: '부산광역시', code: '11H20000'},
+    {name: '울산광역시', code: '11H20000'},
+    {name: '경상남도', code: '11H20000'},
+    {name: '제주도', code: '11G00000'}
+];
+
+var midTempList = fs.readFileSync('./utils/data/region.csv').toString().split('\r\n');
+
+//var midNameList = ['midLand', 'midTemp', 'midSea'];
+var midLandDoc = mongoose.model('midLand', midLandSchema);
+var midTempDoc = mongoose.model('midTemp', midTempSchema);
+
+function createMidLandDoc(){
+    midLandList.forEach(function(line, idx){
+        var doc = new midLandDoc();
+        doc.town.first = line.name;
+        doc.regId = line.code;
+
+        doc.save(function(err){
+            if(err) process.exit(1);
+            if(idx + 1 == midLandList.length) process.exit(0);
+        });
+    });
+}
+
+function createMidTempDoc(){
+    midTempList.forEach(function(line, idx){
+        var doc = new midTempDoc();
+        line.split(',').forEach(function(entry, i){
+            if(i === 0) {doc.town.first = entry; }
+            else if(i === 1) {doc.town.second = entry;}
+            else if(i === 4) {doc.regId = entry;}
+        })
+
+        doc.save(function(err){
+            if(err) process.exit(1);
+            if(idx + 1 == midTempList.length) process.exit(0);
+        });
+    });
+}
+
+function run(){
+    createMidLandDoc();
+    createMidTempDoc();
+}
+
+run();
+
