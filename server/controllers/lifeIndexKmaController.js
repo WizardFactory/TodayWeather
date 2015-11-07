d67a06d05a1fd7db90262e737616c6ea8c6dfe23
aleckim <kimalec7@gmail.com>
2015-11-07 21:01:47 +0900

    #269 #263
    sensorytem 현재시간 없는 경우 3시간후 데이터 사용
    Ultrv가 없는 경우 - 로 표시
    _appendFromKeco에서 callback 두번 호출하는 문제 수정
    lifeIndex를 요청시에 provider로 부터 가지고 오게 수정
    convertDateToHHMM 에서 시간 잘 못 계산하는 부분 수정
    package.json에 main 추가

--- server/controllers/lifeIndexKmaController.js
+++ server/controllers/lifeIndexKmaController.js
@@ -53,53 +53,86 @@ LifeIndexKmaController._addIndexDataToList = function(destList, srcList, indexNa
     return false;
 };
 
-LifeIndexKmaController.appendData = function (town, shortList, midList, callback) {
-    var self = this;
-    //find
+LifeIndexKmaController._appendFromKma = function (town, callback){
+    var keyBox = require('../config/keydata').keyString;
+    var lifeIndexKma = new (require('../lib/lifeIndexKmaRequester'))();
+    lifeIndexKma.setServiceKey(keyBox.pokers);
+    lifeIndexKma.getLifeIndexByTown(town, callback);
+};
+
+LifeIndexKmaController._appendFromDb = function(town, callback) {
     LifeIndexKma.findOne({town:town}, function (err, indexData) {
         if (err) {
             log.error(err);
             return callback(err);
         }
+        if (!indexData) {
+            err = new Error('Fail to find indexData ' + town.toString());
+            log.error(err);
+            return callback(err);
+        }
+        return callback(undefined, indexData);
+    });
+};
 
-        try {
-            if (!indexData) {
-                err = new Error('Fail to find indexData '+town.toString());
-                log.error(err);
-                return callback(err);
-            }
+LifeIndexKmaController._addIndexData = function (indexData, shortList, midList, callback) {
+    var ret = false;
+    var self = this;
+    try {
+        log.debug(indexData.toString());
 
-            var ret = false;
-            log.debug(indexData.toString());
+        for (var k in indexData) {
+            if (indexData[k] && indexData[k].lastUpdateDate) {
 
-            for (var k in indexData) {
-                if (indexData[k] && indexData[k].lastUpdateDate) {
+                log.debug('add ' + k + ' data to list');
 
-                    log.debug('add ' + k + ' data to list');
+                /*lifeIndexKma에서 lastUpdateDate만 있고 data는 저장안된 경우가 있음*/
+                if (indexData[k].data.length === 0) {
+                    log.warn(k + ' data is empty');
+                    continue;
+                }
 
-                    /*lifeIndexKma에서 lastUpdateDate만 있고 data는 저장안된 경우가 있음*/
-                    if (indexData[k].data.length === 0) {
-                        log.warn(k + ' data is empty');
-                        continue;
+                if (indexData[k].data[0].time) {
+                    if (shortList) {
+                        ret = self._addIndexDataToList(shortList, indexData[k].data, k);
                     }
-
-                    if (indexData[k].data[0].time) {
-                        if (shortList) {
-                            ret = self._addIndexDataToList(shortList, indexData[k].data, k);
-                        }
-                    }
-                    else {
-                        if (midList) {
-                            ret = self._addIndexDataToList(midList, indexData[k].data, k);
-                        }
+                }
+                else {
+                    if (midList) {
+                        ret = self._addIndexDataToList(midList, indexData[k].data, k);
                     }
                 }
             }
-
-            callback(undefined, ret);
         }
-        catch(e) {
-            callback(e);
+
+        callback(undefined, ret);
+    }
+    catch(e) {
+        callback(e);
+    }
+};
+
+LifeIndexKmaController.needToUpdate = function(indexData) {
+   //todo: check time
+    if(indexData) {
+        return false;
+    }
+    return true;
+};
+
+LifeIndexKmaController.appendData = function (town, shortList, midList, callback) {
+    var self = this;
+    this._appendFromDb(town, function(err, indexData) {
+        if (err || self.needToUpdate()) {
+            self._appendFromKma(town, function(err, indexData) {
+                if (err) {
+                    return callback(err);
+                }
+                self._addIndexData(indexData, shortList, midList, callback);
+            });
+        }
+        else {
+            self._addIndexData(indexData, shortList, midList, callback);
         }
     });
 };
