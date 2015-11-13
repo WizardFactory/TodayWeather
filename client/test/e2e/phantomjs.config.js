64cbfc115b0a29275fd113bedcd50a326779aa93
Kevin Hwang <neoqmin@gmail.com>
2015-11-13 20:01:34 +0900

    #6 travis 설정&protractor 기본 파일

---
+++ client/test/e2e/phantomjs.config.js
@@ -0,0 +1,20 @@
+var config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['defaults.js'],
+    config['capabilities'] = {
+        browserName: 'phantomjs'
+    },
+    files: [
+        'http://code.jquery.com/jquery-1.9.1.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/chai/3.4.0/chai.js',
+        'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.js',
+        'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular-resource.js',
+        'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular-mocks.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.js',
+        'www/js/app.js',
+        'www/js/controllers.js',
+        'www/js/services.js',
+    ]
+};
+
+exports.config = config;
