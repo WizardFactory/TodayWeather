e2d987bf0470a5b7b8a1621b41fe69af1c259315
Alec Kim <kimalec7@gmail.com>
2015-12-06 17:47:05 +0900

    대림2동, 신공덕동, 사당1동 에러 수정 #358
    미사동, 미사1동, 미사2동 추가
    서울/동작구/사당동 제거
    town.json 만들때, object 사이에 라인변경 추가

--- server/utils/convertTownJson.js
+++ server/utils/convertTownJson.js
@@ -8,7 +8,7 @@ var srcName = './utils/data/base.csv';
 var dstName = './utils/data/town.json';
 
 var lineList = fs.readFileSync(srcName).toString().split('\n');
-var dstList = [];
+var dstStr = '[';
 //remove header
 lineList.shift();
 //remove last blank line
@@ -20,11 +20,15 @@ function convertJsonObject(list) {
         var jsonObject = {"first":srcArray[0],"second":srcArray[1],"third":srcArray[2],
             "lat":parseInt(srcArray[3]),"long":parseInt(srcArray[4]),"areaNo":parseInt(srcArray[5])};
         //console.log(jsonObject);
-        dstList.push(jsonObject);
+        //dstStr.push(jsonObject);
+        dstStr += JSON.stringify(jsonObject) +',\n';
         return true;
     });
 
-    fs.writeFile(dstName, JSON.stringify(dstList), function (err) {
+    dstStr = dstStr.substring(0, dstStr.length - 2);
+    dstStr += ']\n';
+
+    fs.writeFile(dstName, dstStr, function (err) {
         if(err) {
             throw err;
         }
