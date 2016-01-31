f651126d9fb73372fc9abc4bdd28b5c50d4c2ef5
Alec Kim <kimalec7@gmail.com>
2016-01-31 22:39:56 +0900

    release version 0.7.3
    fixed widget error

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -737,7 +737,7 @@ angular.module('starter.controllers', [])
     .controller('SettingCtrl', function($scope, $rootScope, $ionicPlatform, $ionicAnalytics, $http,
                                         $cordovaInAppBrowser) {
         //sync with config.xml
-        $scope.version  = "0.7.2";
+        $scope.version  = "0.7.3";
 
         //it doesn't work after ionic deploy
         //var deploy = new Ionic.Deploy();
