6455ec0fa799641016c97ccca23ed94918ec0ccd
Alec Kim <kimalec7@gmail.com>
2016-03-29 11:42:08 +0900

    release version 0.7.7
    탭 날씨 아이콘 변경
    식중독지수 “경고”이상인 경우 표시
    의견보내기 추가
    android mashmallow에서 폰트 너무 큰 문제 수정.
    상세날씨에 습도 추가.
    데이터 검증부분 서버로 이동하면서 모바일 성능 약간 개선

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -746,7 +746,7 @@ angular.module('starter.controllers', [])
     .controller('SettingCtrl', function($scope, $rootScope, $ionicPlatform, $ionicAnalytics, $http,
                                         $cordovaInAppBrowser) {
         //sync with config.xml
-        $scope.version  = "0.7.6";
+        $scope.version  = "0.7.7";
 
         //it doesn't work after ionic deploy
         //var deploy = new Ionic.Deploy();
