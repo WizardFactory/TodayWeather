be232f3d26b192a8a5bedc56a41322b97d22a482
Alec Kim <kimalec7@gmail.com>
2016-09-20 23:34:45 +0900

    release 0.9.10
    위젯 초기값정리.
    0.9.9
    ionic.io 제거.
    iOS widget server address 오류 수정.
    안드로이드 위젯 7가지 추가
    안드로이드 위젯 자동업데이트, 투명도 조정 기능 추가.
    날씨 데이터 업데이트 시각 추가.
    권한 오류로 무한로딩걸리는 문제 수정.
    화면설명 일부 변경
    
    #970  fixed compile error
    #958  disable setAllowIDFACollection

--- android/src/net/wizardfactory/todayweather/widget/WidgetProviderConfigureActivity.java
+++ android/src/net/wizardfactory/todayweather/widget/WidgetProviderConfigureActivity.java
@@ -5,6 +5,7 @@ import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
