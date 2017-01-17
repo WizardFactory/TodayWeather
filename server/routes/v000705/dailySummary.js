35877cc209ba1f7fcad3daeae182ed9b958652b5
Alec Kim <kimalec7@gmail.com>
2017-01-17 10:48:51 +0900

    unused mergeCurrentByShortest #823 예보시간 변경으로 다가올 shotest는 30분 발표가 아니라, 1시간 30분전 발표 내용이라 제외함.

--- server/routes/v000705/dailySummary.js
+++ server/routes/v000705/dailySummary.js
@@ -26,7 +26,7 @@ function divideParams(req, res, next) {
 
 router.get('/town/*', [divideParams, cTown.getAllDataFromDb, cTown.getShort, cTown.getShortRss, cTown.getShortest,
                                     cTown.getCurrent, cTown.updateCurrentListForValidation, cTown.mergeCurrentByStnHourly, cTown.getKmaStnMinuteWeather,
-                                    cTown.convert0Hto24H, cTown.mergeCurrentByShortest, cTown.mergeShortWithCurrentList,
+                                    cTown.convert0Hto24H, cTown.mergeShortWithCurrentList,
                                     cTown.mergeByShortest, cTown.adjustShort,
                                     cTown.getMid, cTown.getMidRss, cTown.convertMidKorStrToSkyInfo, cTown.getPastMid, cTown.mergeMidWithShort,
                                     cTown.getLifeIndexKma, cTown.getKeco, cTown.getKecoDustForecast,
