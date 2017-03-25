d1e7f23ea1d53e1fcb5925d461d3e5151142f590
pokers <pokers11@empal.com>
2017-03-25 10:36:17 +0900

    #1485 Error handler for ETIMEDOUT
    - error handle “ETIMEDOUT”

--- server/test/testDSF/testDsfRequester.js
+++ server/test/testDSF/testDsfRequester.js
@@ -163,6 +163,34 @@ describe('unit test - DSF', function(){
         });
     });
 
+    it('test error case : ETIMEDOUT', function(done){
+        var dsf = new dsfRequester();
+        var date = '2016-08-11T12:00:00-0900';
+
+        var count = 1;
+
+        dsf.get = function(url, option, callback){
+            if(count-- > 0) {
+                return callback({code:"ETIMEDOUT"});
+            }else{
+                return req.get(url, option, callback);
+            }
+        };
+
+        dsf.getForecast({lat:39.66, lon:116.40}, date, keybox.dsf_keys[0].key, function(err, result){
+            if(err){
+                log.error('!!! failed to get previous weather data');
+                log.error(err);
+                done();
+                return;
+            }
+
+            log.info('!!! Successed to get previous weather data');
+            log.info(result);
+            done();
+
+        });
+    });
 
 /*
     var leadingZeros = function(n, digits) {
