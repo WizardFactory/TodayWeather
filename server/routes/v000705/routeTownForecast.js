9e237cf24699fe8e85646b57911afb9d57a670dc
aleckim <kimalec7@gmail.com>
2017-04-23 03:46:41 +0900

    동일함수 중복호출
    #1114 #970
    
    삭제  동일함수 중복호출

--- server/routes/v000705/routeTownForecast.js
+++ server/routes/v000705/routeTownForecast.js
@@ -66,7 +66,7 @@ router.get('/:region/:city/:town', [cTown.checkParamValidation, cTown.getAllData
                                     cTown.getCurrent, cTown.updateCurrentListForValidation, cTown.mergeCurrentByStnHourly, cTown.getKmaStnMinuteWeather,
                                     cTown.convert0Hto24H, cTown.mergeShortWithCurrentList,
                                     cTown.mergeByShortest, cTown.adjustShort,
-                                    cTown.getMid, cTown.getMidRss, cTown.convertMidKorStrToSkyInfo, cTown.convertMidKorStrToSkyInfo, cTown.getPastMid, cTown.mergeMidWithShort,
+                                    cTown.getMid, cTown.getMidRss, cTown.convertMidKorStrToSkyInfo, cTown.getPastMid, cTown.mergeMidWithShort,
                                     cTown.getLifeIndexKma, cTown.getKeco, cTown.getKecoDustForecast,
                                     cTown.insertIndex, cTown.insertStrForData, cTown.insertSkyIcon,
                                     cTown.getSummary, cTown.dataToFixed, cTown.sendResult]);
