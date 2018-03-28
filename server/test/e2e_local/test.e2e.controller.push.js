0bc556c20d92edfba2d1829d71f8c7b8b634e01d
alec kim <kimalec7@gmail.com>
2018-03-28 09:47:46 +0900

    TW-162 alarm push에 '통합대기 undefined' 전달되는 문제 - arpltn이 없는 경우 발생   - str도 있는지 체크해서 추가하게 변경

--- server/test/e2e_local/test.e2e.controller.push.js
+++ server/test/e2e_local/test.e2e.controller.push.js
@@ -74,6 +74,17 @@ describe('e2e local test - controller push', function() {
         "geo" : [ -79.936, 40.461 ],
         "name" : "New York", "dayOfWeek":[false, true, false, true, false, true, false], "timezoneOffset":540};
 
+    var pushInfo4 = { "id":4, "cityIndex" : 4,
+        "registrationId" : "3c9b9e4f199b94bbf6a5253860c09a33f2dcabcdb097ec6d3f9a7ab44dba013f",
+        "pushTime" : 82900,
+        "enable" : false,
+        "category" : "alarm",
+        "type" : "ios", "source" : "DSF", "lang" : "ko",
+        "units" : { "temperatureUnit" : "C", "windSpeedUnit" : "m/s",
+            "pressureUnit" : "hPa", "distanceUnit" : "km", "precipitationUnit" : "mm" },
+        "geo" : [ 85.324, 27.717 ],
+        "name" : "Kathmandu", "dayOfWeek":[false, true, false, true, false, true, false], "timezoneOffset":540};
+
     it('test request daily summary without town', function(done) {
         this.timeout(20*1000);
         var co = new ControllerPush();
