2dcb2612d67312c97d53b2e2a29a39cddbd1c45f
Alec Kim <kimalec7@gmail.com>
2018-04-27 10:52:22 +0900

    release tw v0.9.95 tw v0.1.3

--- client/www/data/update.info.js
+++ client/www/data/update.info.js
@@ -1,35 +1,30 @@
-window.todayWeather = {appVersion: '0.9.90', enablePopup: true};
+window.todayWeather = {appVersion: '0.9.95', enablePopup: true};
 window.todayWeather.updateInfo = [
     {
         lang: 'en',
         all: [
-            '* new chart for hourly air info',
+            '* new Weahter Picture Theme',
             'have a nice day!'
         ],
-        android: [
-            '* new severe weather alert',
-            '* extend weather alearm'
-        ],
+        android: [],
         ios: []
     },
     {
         lang: 'ko',
         all: [
-            '* 대기정보 페이지의 시간별예보 - 차트로 업데이트',
-            '* 전국날씨페이지 스크롤 지원',
+            '* new 날씨 사진 테마',
+            '* 대기정보페이지에 날씨 요약추가',
+            '* 대기정보페이지에 인근측정소정보 추가',
             '즐거운 하루 되세요. ^^'
         ],
         android: [
-            '* new 눈/비 알림, 미세먼지 나쁨 알림!',
-            '* 한지역에 대하여 5개까지 알람 설정 가능하게 확장'
+            '* new back key 종료방지'
         ],
-        ios: [
-            '* widget 대기정보 컬러조정'
-        ]
+        ios: []
     }
 ];
 
-window.todayAir = {appVersion: '0.1.0', enablePopup: true};
+window.todayAir = {appVersion: '0.1.3', enablePopup: true};
 window.todayAir.updateInfo = [
     {
         lang: 'en',
