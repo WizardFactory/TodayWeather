6c31f44b0e9178f2a227007fec4d885765bc5c37
alec kim <kimalec7@gmail.com>
2018-06-10 16:58:29 +0900

    TW-335 add nullscholl wind link

--- client/www/js/controller.settingctrl.js
+++ client/www/js/controller.settingctrl.js
@@ -28,12 +28,30 @@ angular.module('controller.settingctrl', [])
             if (menu === 'sendMail') {
                 $ionicSideMenuDelegate.toggleLeft();
                 Util.sendMail($translate);
-            } else if (menu === 'openMarket') {
+            }
+            else if (menu === 'openMarket') {
                 $ionicSideMenuDelegate.toggleLeft();
                 Util.openMarket();
-            } else if (menu === 'openInfo') {
+            }
+            else if (menu === 'openInfo') {
                 openInfo();
-            } else {
+            }
+            else if (menu === 'nullschool') {
+                Util.ga.trackEvent('action', 'click', 'open nullschool');
+                var src = "https://earth.nullschool.net/#current/wind/surface/level/orthographic=-233.36,38.96,3000";
+                if (window.cordova && cordova.InAppBrowser) {
+                    cordova.InAppBrowser.open(src, "_system");
+                }
+                else {
+                    var options = {
+                        location: "yes",
+                        clearcache: "yes",
+                        toolbar: "no"
+                    };
+                    window.open(src, "_blank", options);
+                }
+            }
+            else {
                 $ionicSideMenuDelegate.toggleLeft();
                 $location.path('/' + menu);
             }
