26182bff424500c3e6579b885faca5a2ca404651
skywlrl <skywlrl@gmail.com>
2018-07-29 15:15:10 +0900

    TW-97 시간별 습도, 풍향, 풍속 정보 표시 #1852 * short-detail-chart의 표시 상태를 TwStorage에 저장  - expander를 click 시 변경된 상태 저장  - 앱 실행 시 저장된 상태값으로 short-detail-chart 표시 여부 설정  - 기본값은 short-detail-chart가 숨겨진 상태임 * short-detail-chart의 데이터는 3시간 단위의 정각에 표시되도록 수정

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -1029,13 +1029,13 @@ angular.module('starter', [
                                 }
                             })
                             .attr('x1', function (d, i) {
-                                return x.rangeBand() * i + x.rangeBand() / 2+0.5;
+                                return x.rangeBand() * i + x.rangeBand() / 2 + 0.5;
                             })
                             .attr('x2', function (d, i) {
-                                return x.rangeBand() * i + x.rangeBand() / 2+0.5;
+                                return x.rangeBand() * i + x.rangeBand() / 2 + 0.5;
                             })
                             .attr('y1', 0)
-                            .attr('y2', height);
+                            .attr('y2', margin);
 
                         guideLines.exit().remove();
 
@@ -1053,19 +1053,19 @@ angular.module('starter', [
                                 return scope.iconsImgPath + "/wind_direction.png";
                             })
                             .attr("x", function (d, i) {
-                                return x.rangeBand() * i - scope.smallImageSize/2;
+                                return x.rangeBand() * i + x.rangeBand() / 2 - scope.smallImageSize/2;
                             })
                             .attr("y", margin / 2)
                             .attr("width", scope.smallImageSize)
                             .attr("height", scope.smallImageSize)
                             .attr('transform', function (d, i) {
-                                return 'rotate(' + d.value.vec + ', ' + (x.rangeBand() * i) + ', ' + (margin + scope.smallImageSize) / 2 + ')';
+                                return 'rotate(' + d.value.vec + ', ' + (x.rangeBand() * i + x.rangeBand() / 2) + ', ' + (margin + scope.smallImageSize) / 2 + ')';
                             });
 
                         hourObject.append("text")
                             .attr('class', 'chart-text')
                             .attr("x", function (d, i) {
-                                return x.rangeBand() * i;
+                                return x.rangeBand() * i + x.rangeBand() / 2;
                             })
                             .attr("y", function(){
                                 return margin / 2 + scope.smallImageSize + textTop;
@@ -1085,7 +1085,7 @@ angular.module('starter', [
                                 return scope.iconsImgPath + "/humidity_" + reh +".png";
                             })
                             .attr("x", function (d, i) {
-                                return x.rangeBand() * i - scope.smallImageSize/2;
+                                return x.rangeBand() * i + x.rangeBand() / 2 - scope.smallImageSize/2;
                             })
                             .attr("y", function () {
                                 return margin / 2 + scope.smallImageSize + textTop + margin;
@@ -1096,7 +1096,7 @@ angular.module('starter', [
                         hourObject.append("text")
                             .attr('class', 'chart-text')
                             .attr("x", function (d, i) {
-                                return x.rangeBand() * i;
+                                return x.rangeBand() * i + x.rangeBand() / 2;
                             })
                             .attr("y", function () {
                                 return margin / 2 + scope.smallImageSize * 2 + textTop + margin + textTop;
@@ -1107,10 +1107,6 @@ angular.module('starter', [
                             .append('tspan')
                             .attr('class', 'chart-unit-text')
                             .text('%');
-
-                        hourObject.filter(function(d, i) {
-                            return i == 0;
-                        }).remove();
                     };
 
                     scope.$watch('timeWidth', function(newValue) {
